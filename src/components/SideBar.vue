12616a425f955c65a31fdf931731fbb300ff11cb
xugw <2279799268@qq.com>
2022-05-15 22:29:23 +0800

    添加路由

--- src/components/SideBar.vue
+++ src/components/SideBar.vue
@@ -1,37 +1,29 @@
 <template>
   <el-menu class="el-menu-vertical-demo hidden-sm-and-down" :collapse="isCollapse" background-color="#545c64"
     text-color="#fff" active-text-color="#ffd04b">
-    <el-menu-item index="1">
+    <el-menu-item index="1" @click="linkRoute('home')">
       <i class="el-icon-s-home"></i>
       <span slot="title">首页</span>
     </el-menu-item>
-    <el-menu-item index="2">
+    <el-menu-item index="2" @click="linkRoute('note')">
       <i class="el-icon-collection"></i>
       <span slot="title">笔记</span>
     </el-menu-item>
-    <el-menu-item index="3">
+    <el-menu-item index="3" @click="linkRoute('code')">
       <i class="el-icon-notebook-1"></i>
-      <span slot="title">文章</span>
+      <span slot="title">代码</span>
     </el-menu-item>
     <el-submenu index="4">
       <template slot="title">
         <i class="el-icon-document"></i>
-        <span slot="title">代码</span>
+        <span slot="title" @click="linkRoute('article')">文章</span>
       </template>
       <el-menu-item-group>
-        <span slot="title">分组一</span>
-        <el-menu-item index="1-1">选项1</el-menu-item>
-        <el-menu-item index="1-2">选项2</el-menu-item>
+        <el-menu-item index="4-1" @click="linkRoute('project')">开源项目</el-menu-item>
+        <el-menu-item index="4-2" @click="linkRoute('software')">软件工具</el-menu-item>
       </el-menu-item-group>
-      <el-menu-item-group title="分组2">
-        <el-menu-item index="1-3">选项3</el-menu-item>
-      </el-menu-item-group>
-      <el-submenu index="1-4">
-        <span slot="title">选项4</span>
-        <el-menu-item index="1-4-1">选项1</el-menu-item>
-      </el-submenu>
     </el-submenu>
-    <el-menu-item index="5">
+    <el-menu-item index="5" @click="linkRoute('link')">
       <i class="el-icon-link"></i>
       <span slot="title">链接</span>
     </el-menu-item>
@@ -46,6 +38,11 @@ export default {
       return this.$store.state.HandleCollapse.isCollapse
     }
   },
+  methods: {
+    linkRoute(item) {
+      this.$router.push({ name: item })
+    }
+  }
 }
 </script>
 
--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,11 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import NoteView from '../views/NoteView.vue'
+import ArticleView from '../views/ArticleView.vue'
+import CodeView from '../views/CodeView.vue'
+import LinkView from '../views/LinkView.vue'
+import ArticleProject from '../views/ArticleProject.vue'
+import ArticleSoftware from '../views/ArticleSoftware.vue'
 
 Vue.use(VueRouter)
 
@@ -15,7 +20,34 @@ const routes = [
     path: '/note',
     name: 'note',
     component: NoteView
-  }
+  },
+  {
+    path: '/article',
+    name: 'article',
+    component: ArticleView,
+    children: [
+      {
+        path: 'project',
+        name: 'project',
+        component: ArticleProject
+      },
+      {
+        path: 'software',
+        name: 'software',
+        component: ArticleSoftware
+      },
+    ]
+  },
+  {
+    path: '/code',
+    name: 'code',
+    component: CodeView
+  },
+  {
+    path: '/link',
+    name: 'link',
+    component: LinkView
+  },
 ]
 
 const router = new VueRouter({
